fabc89efa4a278c6d654d29f6447bdc0dfff6f9a
xiaoxin <17265290913@163.com>
2026-04-11 01:07:08 +0800

    fix: 允许 SVG 图片加载(placehold.co)

--- next.config.js
+++ next.config.js
@@ -28,6 +28,9 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 60 * 60 * 24, // 缓存 24 小时
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   async headers() {
     return [
